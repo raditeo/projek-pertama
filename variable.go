952d738ad56cb3a2a533867196909fc65376cab4
raditeo <raditeo@gmail.com>
2022-03-21 11:29:21 +0700

    Initial commit

---
+++ variable.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nama = "golang"
+	// var age = 5
+	// var address = "Google"
+
+	// fmt.Printf("Halo namaku %s, umurku %d, dan aku berasal dari %s", nama, age, address)
+
+	// var nama string
+	// var age int
+	// nama = "test"
+	// age = 5
+
+	// fmt.Println("ini adalah bahasa pemrograman: ", nama)
+	// fmt.Println("umur saya: ", age)
+	// fmt.Printf("%T, %T", nama, age)
+
+	// var student1, student2, student3 string = "sadsad", "werwer", "sdfdsf"
+	// var first, second, third int = 1, 2, 3
+
+	// fmt.Println(student1, student2, student3)
+	// fmt.Println(first, second, third)
+
+	var test string
+	var test1, test2, test3 = "Golang", 5, "Google"
+
+	_, _, _, _ = test, test1, test2, test3
+
+	fmt.Println(test, test1)
+}
